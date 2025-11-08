b5155b7690d71fa949fe76526e284d0158c0bc53
Sourin Mukherjee <sourinm81@gmail.com>
2025-11-08 20:36:57 +0530

    Add files via upload

---
+++ Day 1/DataType.py
@@ -0,0 +1,81 @@
+a=5
+b=7
+print(a+b)
+
+s=6
+b=9
+c=s+b
+print("sum=",c)
+
+def a(a,b):
+    c=a+b
+    print("the value of c is",c)
+a(3,4)
+
+
+def calculate(a,b):
+    c=a+b
+    d=a-b
+    e=a*b
+    f=a/b 
+    print(c)
+    print(d)
+    print(e)
+    print(f)
+calculate(9,6);    
+
+def integer(a):
+    print (int(a))
+integer(5)  ##here 5 is the integer value .
+integer(87.67)
+def floet(a):
+    print (float(a))
+floet(7.9)  ##here 7.9 is the floting type number.
+floet(6)   ##in python is shows 6.0
+
+def a( a):
+    print(int (a))
+    print(float(a))
+a(89)    
+
+##bool number
+
+def value(a):
+    print(bin(a))
+    print(hex(a))
+    print(oct(a))
+value(56)
+
+def rev(a):
+   print(int('0b111000',2))
+   print(int('0x38',16))
+   print(int('0o70',8))
+rev(56)   
+
+# def a(a,b):
+#     if(a<b):
+#         print("a is lessthan b")
+#     else:
+#         print("b is lessthan a")
+# a(4,5)
+# a(7,4)       
+
+def value(a, b):
+    if a < b:
+        print("a is less than b")
+    elif a > b:
+        print("a is greater than b")
+    else:
+        print("a is equal to b")
+value(8.0, 0.9)
+# value(8.0,0.9)
+# value(8,9)
+# value(8,9)
+# value(8,8)  
+
+     
+
+
+  
+  
+        
