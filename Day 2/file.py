780db975c6a584d01b1b8d105679626234cae6ac
Sourin Mukherjee <sourinm81@gmail.com>
2025-11-09 17:23:31 +0530

    Add files via upload

---
+++ Day 2/file.py
@@ -0,0 +1,64 @@
+str="good morning"
+# print(str)
+
+str1='''hey this is a 
+        good python data
+        the good catch or the good line'''
+# print(str1[0:])  
+# print(str1[0:2])        
+# print(str1[0:5])        
+# print(str1[0:10]) 
+# print(str1[-7:])        
+       
+      
+# print(str1)
+
+
+str2="""Hye "sourin" , you are a bad teacher."""
+# print(str2)
+
+str3='''hey the real hero is "baba-ma"'''
+# print(str3)
+
+
+str = "HelloPython"
+# print(str[-9:-8])
+# print(str[-9:-7])
+# print(str[-9:-6])
+# print(str[-11:-10])
+
+
+##array(list)
+
+a=[3,46,7,8,9]
+# print(a)
+
+b=["gfg",8,'gh',8.0,1001]
+# print(b)
+# print(b[0:])
+# print(b[0:3])
+# print(b[-3:-1])
+
+
+c=[5,6,7,8,9]
+# print(bytes(c))
+
+d=['\x05\x06\x07\x08\t']
+# print(list(d[0].encode()))
+
+# def a(n):
+    # print(bin(n))
+    # print(hex(n))
+    # print(oct(n))
+# a(78)    
+
+
+# print(int('0b1001110',2))
+
+    
+
+
+
+
+
+
