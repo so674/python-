2dadefd08dab4394fd3a37cde67e91655683962d
Sourin Mukherjee <sourinm81@gmail.com>
2025-11-10 18:19:47 +0530

    Add files via upload

---
+++ Day 3/flie.py
@@ -0,0 +1,37 @@
+def add(a,b):
+    print(a+b)
+# add(3,4)
+
+def sub(x,y):
+    print(x-y)
+# sub(9,6)
+
+def multiply(p,q):
+    print(p*q)
+# multiply(5,6)    
+
+def div(a,b):
+    print(int(a/b))
+# div(6,2)   
+
+def  div(a,b):
+    print(float(a/b))
+# div(6,2) 
+
+def  div(a,b):
+    print(a/b)
+# div(6,2)  
+
+def module(a,b):
+    print(a%b)
+# module(6,1)     
+
+def exponent(a,b):
+    print(a**b)
+# exponent(4,2)   
+
+def integer_divition(a,b):
+    print(a//b)
+# integer_divition(10,2)     
+
+            
