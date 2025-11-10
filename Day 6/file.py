056050d944cf0c1bd86387c3c7908caa9a09c253
Sourin Mukherjee <sourinm81@gmail.com>
2025-11-10 21:54:05 +0530

    Add files via upload

---
+++ Day 6/file.py
@@ -0,0 +1,17 @@
+# x=100
+# y=600
+# print(x and y)
+
+# print(0 and 5)      # → 0 (first is false)
+# print(5 and 0)      # → 0 (second is false)
+# print(5 and 7)      # → 7 (both true)
+# print(0 and 0)      # → 0 (first false)
+
+def And(a,b):
+    print(a and b)
+# And(5,2)    
+# And(5,0)
+And(0,2)    
+  
+  
+
