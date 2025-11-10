997b4107279ac0acfe4e7f20ee700b95f9abe840
Sourin Mukherjee <sourinm81@gmail.com>
2025-11-10 19:32:39 +0530

    Add files via upload

---
+++ Day 5/file.py
@@ -0,0 +1,35 @@
+def A(a,b):
+    if(a==b):
+        print("a is equal to b")
+    else:
+        print("a is  not equal to b")
+# a=int(input("enter the a:"))
+# b=int(input("enter the b:"))
+# A(a,b)
+
+
+def A(a,b):
+    if(a<b):
+        print("a is lessthan b")
+    else:
+        print("a is graterthen b")    
+# A(6,7)
+# A(9,3) 
+
+
+def A(a,b):
+    if(a!=b):
+        print("A is not equal to b")
+    else:
+        print("A is Equal to B")
+# A(6,6)
+# A(7,5)   
+
+
+def A(a,b):
+    if(a<=b):
+        print("a is lessthan equal to b")
+    else:
+        print("a is graterthan equal to b")
+# A(7,8) 
+# A(9,1)                                 
