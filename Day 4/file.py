74e0cd8e21669485339555b7093536474dfc6636
Sourin Mukherjee <sourinm81@gmail.com>
2025-11-10 18:30:58 +0530

    Add files via upload

---
+++ Day 4/file.py
@@ -0,0 +1,30 @@
+def A(a):
+    a+=2  #a=a+2
+    print(a)
+# A(6)    
+
+def B(a):
+    a-=5  #a=a-5
+    print(a)
+# B(9)   
+
+def C(a):
+    a*=2
+    print(a)
+# C(6)     
+
+def D(a):
+    a/=2
+    print(a)
+# D(10)  
+
+def D(a):
+    a//=2
+    print(a)
+# D(10)
+
+def E(a):
+    a%=2
+    print(a)
+# E(10)    
+  
