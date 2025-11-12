ce5cf338fd1ce2d4af47530d0801a45c3b147772
Sourin Mukherjee <sourinm81@gmail.com>
2025-11-12 21:38:14 +0530

    Add files via upload

---
+++ Day 9/file.py
@@ -0,0 +1,24 @@
+def Not(a):
+    print(~a)
+# Not(7)    #(~a=-(a+1))
+
+def b(a):
+    print(~a)
+# b(6)    
+
+x = 4
+# print(~x)
+a = 15
+b = ~a
+# print(a & b)
+# print(a | b)
+# print(a ^ b)
+
+def a(num):
+    b=num
+    comp=~num
+    print("original number:"b)
+    
+    
+    
+    
